b8cdaa6ec463b2c6ca4af028f41332c281971b07
Ubuntu <ubuntu@ip-172-31-91-115.ec2.internal>
2025-07-12 12:27:56 +0000

    added terraform infrascan

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,29 @@
 resource "aws_s3_bucket" "example" {
   bucket = "my-devsecops-demo-bucket"
   acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    target_bucket = "dummy-logging-bucket"
+    target_prefix = "logs/"
+  }
+
+  lifecycle_rule {
+    enabled = true
+    noncurrent_version_expiration {
+      days = 30
+    }
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
